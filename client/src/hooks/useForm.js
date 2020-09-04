0a23cde7b63911705218da5b161c1270bf76bda0
Beau Bradley <beaubradley@outlook.com>
2020-09-04 12:06:02 -0400

    mvp

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,22 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react';
+
+export const useForm = (initialValues, submitCallback) => {
+    const [values, setItemValues] = useState(initialValues);
+
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+    
+    const setValues = (newValue) => {
+        setItemValues(newValue)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        submitCallback(true);
+    };
+
+    return [values, setValues, handleChanges, handleSubmit];
+} 
